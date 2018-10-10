00379baa692d0f3cfb845bcc7529eb2c2123f64a
GasparAcevedoZainSouthworks <gaspar.acevedo@southworks.com>
2018-10-10 09:38:17 -0300

    NetCore Console project + .gitignore file were added

---
+++ NET_Autofac/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NET_Autofac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
